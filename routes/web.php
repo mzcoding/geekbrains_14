4020130cd5910c8f6b66c8c40a6c91f97a07dd01
Stanislav Boyko <s.boyko@app-smart.ru>
2022-01-13 21:39:35 +0300

    init commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Admin\NewsController as AdminNewsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//news
+Route::group(['as' => 'admin.', 'prefix' => 'admin'], function() {
+   Route::resource('/categories', AdminCategoryController::class);
+   Route::resource('/news', AdminNewsController::class);
+});
+
+Route::get('/news', [NewsController::class, 'index'])
+	->name('news.index');
+Route::get('/news/{id}', [NewsController::class, 'show'])
+	->where('id', '\d+')
+	->name('news.show');
+
